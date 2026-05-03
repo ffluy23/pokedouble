8b6fd3a1a80cb8f2a0712a972ad8224b9b6864cf
ffluy23 <ffluy23@gmail.com>
2026-05-03 23:36:02 +0900

    명

--- js/doubleintro.js
+++ js/doubleintro.js
@@ -4,7 +4,7 @@ import { auth, db } from "./firebase.js"
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"
 import { doc, getDoc, updateDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js"
 
-const BGM_URL = "https://smoggy-amethyst-kdafhcgqwp.edgeone.app/Glacial%20brilliance.mp3"
+const BGM_URL = "https://principal-coral-pd9dwcb4ap.edgeone.app/Waking%20of%20a%20World%20(JP)%20-%20Wuthering%20Waves.mp3"
 
 const BG_LIST = [
   "https://foolish-rose-9l9aoow1vy.edgeone.app/배경1%20(1).jpg",
